e9e4452c659237a988b98df292227955985b26db
Tomi Helin <tomi.helin@student.lut.fi>
2025-01-20 08:49:13 +0200

    task 4 fixes

--- src/components/MyContainer.tsx
+++ src/components/MyContainer.tsx
@@ -3,19 +3,18 @@ import MyList, {TItem} from './MyList'
 
 const MyContainer: React.FC = () => {
     const header: string = 'this is list header'
-    const [items, setItems] = useState<TItem[]>([
-        {id: '1', text: 'This is first task', clicked: false},
-        {id: '2', text: 'this is second task', clicked: false}
-    ])
+    const [items, setItems] = useState<TItem[]>([])
+    const [content, setContent] = useState<string>("")
  
     const addItem = () => {
-        const newItem = {id: 'test', text: 'test', clicked: false}
+        const newItem = {id: Date.now().toString(), text: content, clicked: false};
         setItems([...items, newItem]);
+        setContent("")
     }
 
     return (
         <div>
-            <textarea name="text" id="test"></textarea>
+            <textarea name="text" id="test" onChange={(e) => setContent(e.target.value)} value={content} />
             <button onClick={addItem}>Add</button>
             <MyList header={header} items= {items} />
         </div>
